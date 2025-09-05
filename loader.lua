1f69b63966cbeb4762786284459a28c765b18e5a
SCPShadow <84786285+SCPShadow@users.noreply.github.com>
2025-09-05 20:07:49 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,6 @@
 -- just a simple loader, feel free to exec scripts manually
-if game.PlaceId == 9476339275 then
+if game.PlaceId == 9476339275 then -- double down
     loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/63fc26292ad3442f8315016a03c56136"))()
-elseif game.PlaceId == 122764594952227 then
-    loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/695234330e0f4ae58c1abea876717159"))()
+elseif game.PlaceId == 122764594952227 then -- hvh
+    loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/fa0a20f9c44148e0aea81f813ad7562d"))()
 end
