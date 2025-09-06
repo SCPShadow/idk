569b34ffdee89887c9e91a9ef1c9539ce94ef52c
SCPShadow <84786285+SCPShadow@users.noreply.github.com>
2025-09-06 13:49:20 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,7 +2,7 @@
 if game.PlaceId == 9476339275 then -- double down
     loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/63fc26292ad3442f8315016a03c56136"))()
 elseif game.PlaceId == 122764594952227 then -- hvh
-    loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/081637e1607f43e69035c71d1cad44b7"))() -- bypass
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SCPShadow/idk/refs/heads/main/akufucku.lua"))() -- bypass
     task.wait(1)
     loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/4efb8657710c41f58547842349cde5b1"))()
 end
